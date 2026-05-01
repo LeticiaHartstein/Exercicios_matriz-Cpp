8bea689b8505a571b9ee775b7ad3e4c327b76968
Letcia Hartstein Ivnaov <119649477+LeticiaHartstein@users.noreply.github.com>
2026-05-01 13:49:14 -0300

    Enhance code readability with comments
    Added comments to clarify the purpose of variables and loops.

--- exercicio1/resolucao.cpp
+++ exercicio1/resolucao.cpp
@@ -2,20 +2,31 @@
 using namespace std;
 int main()
 {
+	//criando váriaveis
 	int tab_n = 0, pecas = 0, num = 0;
 	cin >> tab_n;
 	int tabuleiro[tab_n];
+	//loop para inserir as peças
 	for (int i = 0; i < tab_n; i++)
 	{
 		cin >> pecas;
 		tabuleiro[i]  = pecas;
 	}
+	//loop para verificar o campo
 	for (int i = 0; i < tab_n; i++)
 	{
 		num = 0;
+		//verificar as posições das minas
+		//
+		//
+		//se a célula verificada for uma mina adiciona +1 em num
 		if (tabuleiro[i] == 1) num++;
+		//se a célula que antecede a atual for uma mina adicionar +1 em num
 		if (tabuleiro[i-1] == 1) num++;
+		//se a célula que sucede a atual for uma mina adicionar +1 em num
 		if (tabuleiro[i+1] == 1) num++;
+
+		//resultado da análise apenas para tabuleiro[i]
 		cout << "\n" << num;
 	}
 }
