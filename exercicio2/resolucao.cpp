440ceadc73275f3bb285b77141f0d7a595dc8bd2
Letcia Hartstein Ivnaov <119649477+LeticiaHartstein@users.noreply.github.com>
2026-05-01 16:15:01 -0300

    Enhance code readability with comments
    Added comments to clarify variable purposes and loop functionality.

--- exercicio2/resolucao.cpp
+++ exercicio2/resolucao.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
+	//criando váriaveis, o gan tem o maior valor para que dado sempre sejá menor em sua primeira comparação 
 	int competidor = 0, voltas = 0, dado = 0, gan = 10000000000000, compet = 0;
 	cin >> competidor >> voltas;
 	int tabela[competidor][voltas];
+	//loop para adicionar informações de voltas para cada competidor
 	for (int y =0; y< competidor; y++)
 	{
 	    for (int x =0; x < voltas; x++)
@@ -13,6 +15,7 @@ int main()
 	        tabela[y][x] = dado;
 	    }
 	}
+	//loop para encontrar o vencerdor(o mais rápido)
 	for (int comp = 0; comp < competidor; comp++)
 	{
 	   dado = 0;
@@ -26,5 +29,7 @@ int main()
 	       compet = comp;
 	   }
 	}
+	//exibindo
 	cout << "\n" <<compet+1;
+	
 }
