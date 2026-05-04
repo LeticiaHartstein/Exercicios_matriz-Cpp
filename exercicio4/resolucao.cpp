e77b21890594f4b25dfa30556b32d745cfded85e
Letcia Hartstein Ivnaov <119649477+LeticiaHartstein@users.noreply.github.com>
2026-05-03 21:52:33 -0300

    Enhance code clarity with comments
    Added comments to clarify variable purposes and code functionality.

--- exercicio4/resolucao.cpp
+++ exercicio4/resolucao.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main()
 {
+    //t = tempo, a = aceleração, e = espaço
     int t = 0, a = 0, e = 0;
+    //armazenar diferentes tempos, espaço e as diferentes velocidades entre dois pontos
     int tempo[4] = {0}, espaco[4]={0}, velocidade[3] ={0};
+
+    
     cout << "\n Calculo de aceleração e tempo \n";
+    
+    //loop para adicionar informações aos arrays espaco  e velocidade
     for (int i = 0; i < 4; i++)
     {
         cout << "\n Insira o espaco: ";
@@ -16,11 +22,13 @@ int main()
         cin >> e;
         tempo[i] = e;
     }
+    //calculando a velocidade entre dois pontos
     for (int i = 0; i < 3; i++)
     {
             velocidade[i] = (espaco[i+1] - espaco[i])/(tempo[i+1] - tempo[i]);
             cout << "\n" << espaco[i] <<" "<< espaco[i+1] << "=" << velocidade[i] << "M/s"; 
     }
+    //calculando a aceleração entre a 2 velocidade armazenada e a 1 velocidade armazenada
     a = (velocidade[1] - velocidade[0])/( tempo[1] - tempo[0]);
 
     cout << "\n Aceleracao: " << a << "m/s^2";
